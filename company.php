5ed16876eeb39bfdef490ee7f413c881a010365a
artidition <yana-lozovaya@mail.ru>
2017-08-25 21:09:34 +0400

    о компании

--- company.php
+++ company.php
@@ -1,3 +1,16 @@
 <?php include 'header.php'; ?>
+<section id="about-photo">
+	<div class="container"></div>
+</section>
+<section id="bout-info">
+	<div class="container">
+		<h2>
+			О компании
+		</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod facere nam, sunt minus eveniet, perferendis dicta! Illum, alias, modi. Placeat possimus, eligendi consequatur voluptatibus fuga magni fugit mollitia saepe a reiciendis officiis libero assumenda expedita eaque provident eum modi tempore alias? Voluptatibus quidem vitae illo corporis? Quod odit eius ipsum recusandae nulla consequuntur cupiditate doloremque velit aspernatur, temporibus reiciendis quisquam quidem optio, commodi nostrum blanditiis. Cum odio, ducimus corporis exercitationem delectus! Officiis accusamus repellendus sed commodi labore necessitatibus vitae saepe mollitia animi similique fugit in libero rerum qui impedit hic cumque quis sequi minima quia, culpa voluptatibus repellat illum. Tenetur mollitia deleniti cupiditate aliquid quaerat ut, consequatur eius modi voluptatum ipsam nostrum aperiam tempore excepturi suscipit repellendus consequuntur unde ullam. Delectus eaque facere perspiciatis suscipit quia tempore totam alias, praesentium deleniti explicabo iure quo, possimus vitae mollitia deserunt id repellendus aliquid provident illo enim obcaecati vel sit? Voluptas distinctio aspernatur incidunt ut, est pariatur enim eius sunt aut reiciendis, totam accusamus. Laboriosam eius, quae! Perferendis blanditiis a nisi amet ducimus, dignissimos deserunt itaque minus quaerat fugit et aliquid in ullam accusamus pariatur. Quae accusamus pariatur sint consectetur, consequatur quod officiis molestias aperiam dolores. Accusantium alias aliquam veritatis animi saepe similique sunt provident deleniti voluptatibus magnam amet quo vel a corporis perferendis officia ab molestiae, earum quibusdam, eum, adipisci repellat natus. Hic quaerat pariatur debitis molestias sequi nostrum, et minus enim nisi accusamus ut ducimus ea, assumenda soluta sit! Amet unde, in rerum impedit beatae accusamus quod. Voluptas repellat, quasi praesentium. Laborum ab aliquid praesentium, magnam quibusdam voluptate. Architecto eaque excepturi consequuntur perspiciatis voluptatibus minima eius ducimus, minus vitae voluptatem officia dolore, neque impedit. Ipsa libero odit, officiis, cum officia inventore, ea hic minima rem ratione quae consequatur quis molestiae voluptatem amet assumenda minus! Quo molestias sint praesentium minima, doloribus maiores.
+		</p>
+	</div>
+</section>
 
 <?php include 'footer.php'; ?>
--- header.php
+++ header.php
@@ -38,7 +38,7 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li>
-                        <a href="#">О компании</a>
+                        <a href="company.php">О компании</a>
                     </li>
                     <li>
                         <a href="#">Информация для туристов</a>
